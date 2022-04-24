87e92037f18e4b2ec819008af308fdbfe0597797
priya shankar <preiyah@gmail.com>
2022-04-24 21:12:48 +0200

    test with patch request

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const ACCESS_TKN = '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';
+const ACCESS_TKN = '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';
 const url = 'https://api.github.com/users/impriyashankar';
 
 const ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36';
+
+let headers = new Headers();
+
+  headers.append('Authorization', ACCESS_TKN);
+  headers.append('User-Agent',ua);
+  headers.append('Content-Type','application/json');
 
 const func = () => {
   const repoP = document.querySelector('#repos');
   const imgAvatar = document.querySelector('.avatar');
 
-  let headers = new Headers();
 
-  headers.append('Authorization', ACCESS_TKN);
-  headers.append('User-Agent',ua);
 
   console.log("github app");
   fetch(url,{
@@ -18,11 +21,51 @@ const func = () => {
   })
   .then(response => response.json())
   .then(data => {
+    //console.log(data);
     repoP.innerText = `My repositories: ${data.public_repos}`;
     imgAvatar.setAttribute('src',`${data.avatar_url}`);
     imgAvatar.setAttribute('width','200px');
+    //display the new name of the latest repo
+    displayLatest();
+
 
   });
 }
 
 func();
+
+const displayLatest =() => {
+  let latestRepo ={};
+  fetch('https://api.github.com/users/impriyashankar/repos?sort=created&direction=desc',{
+    headers: headers
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data[0].name);
+      renameRepo(data[0].name);
+
+
+      //document.querySelector('#latestRepo').innerText = `My latest repository: ${data[0].name}`;
+
+
+
+    });
+
+}
+
+const renameRepo =(repoOldName) => {
+  //console.log(repoOldName);
+  fetch(`https://api.github.com/repos/impriyashankar/${repoOldName}`,{
+    method: 'PATCH',
+    headers: headers,
+    body: JSON.stringify({
+      name: 'github-app'
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  });
+
+
+}
